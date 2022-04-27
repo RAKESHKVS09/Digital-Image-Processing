014578a23a3ddb0daaab1cd8144adf65eb3306c6
RAKESHKVS09 <97229360+RAKESHKVS09@users.noreply.github.com>
2022-04-28 05:20:02 +0530

    Add files via upload

---
+++ DIP-ASSIGNMENT19(Code & outputs)/DIP-Assignment19.py
@@ -0,0 +1,15 @@
+#Laplacian filter
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from PIL import Image, ImageFilter
+image2 = cv2.imread('jeff.JPG') # reads the image
+new_image = cv2.Laplacian(image2,cv2.CV_64F)
+plt.figure(figsize=(11,6))
+plt.subplot(131), plt.imshow(image2, cmap='gray'),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(132), plt.imshow(new_image, cmap='gray'),plt.title('Laplacian')
+plt.xticks([]), plt.yticks([])
+plt.subplot(133), plt.imshow(image2 + new_image, cmap='gray'),plt.title('Resulting image')
+plt.xticks([]), plt.yticks([])
+plt.show()
